c7d1583bff40e316d23bc0f4eb9c0985823bba51
JosephIgbaji <igbajijoseph.k@gmail.com>
2024-01-23 12:07:45 +0100

    overlay tabs added for product

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,11 +4,13 @@ import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import ButtonPrimary from "../ButtonPrimary";
+import NavProduct from "./NavProduct";
 
 const Header = () => {
   const [showMobileNav, setShowMobileNav] = useState(false);
+
   return (
-    <header className="md:px-32 px-5 py-5">
+    <header className="md:px-32 px-5 py-5 relative">
       <nav className="flex items-center justify-between">
         <div>
           <Link href="/">
@@ -28,27 +30,30 @@ const Header = () => {
           >
             About Us
           </Link>
-          <Link
-            className="p-3 flex items-center gap-2 hover:text-primary active:text-primary focus:text-primary hover:bg-primary rounded-lg hover:bg-opacity-20 active:bg-opacity-20 focus:bg-opacity-20 active:bg-primary focus:bg-primary"
-            href="/products"
-          >
-            Products {""}
-            <svg
-              width="12"
-              height="6"
-              viewBox="0 0 12 6"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
+          <div className="relative group">
+            <NavProduct />
+            <Link
+              className="p-3 flex items-center gap-2 hover:text-primary active:text-primary focus:text-primary hover:bg-primary rounded-lg hover:bg-opacity-20 active:bg-opacity-20 focus:bg-opacity-20 active:bg-primary focus:bg-primary"
+              href=""
             >
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M0.414376 0.531506C0.673133 0.20806 1.1451 0.155619 1.46855 0.414376L6.00003 4.03956L10.5315 0.414376C10.855 0.155619 11.3269 0.20806 11.5857 0.531506C11.8444 0.854953 11.792 1.32692 11.4685 1.58568L6.46855 5.58568C6.19464 5.80481 5.80542 5.80481 5.53151 5.58568L0.531506 1.58568C0.20806 1.32692 0.155619 0.854953 0.414376 0.531506Z"
-                fill="black"
-                fillOpacity="0.5"
-              />
-            </svg>
-          </Link>
+              Products {""}
+              <svg
+                width="12"
+                height="6"
+                viewBox="0 0 12 6"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M0.414376 0.531506C0.673133 0.20806 1.1451 0.155619 1.46855 0.414376L6.00003 4.03956L10.5315 0.414376C10.855 0.155619 11.3269 0.20806 11.5857 0.531506C11.8444 0.854953 11.792 1.32692 11.4685 1.58568L6.46855 5.58568C6.19464 5.80481 5.80542 5.80481 5.53151 5.58568L0.531506 1.58568C0.20806 1.32692 0.155619 0.854953 0.414376 0.531506Z"
+                  fill="black"
+                  fillOpacity="0.5"
+                />
+              </svg>
+            </Link>
+          </div>
           <Link
             className="p-3 flex items-center gap-2 hover:text-primary active:text-primary focus:text-primary hover:bg-primary rounded-lg hover:bg-opacity-20 active:bg-opacity-20 focus:bg-opacity-20 active:bg-primary focus:bg-primary"
             href="/services"
---
+++ src/components/header/NavServices.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NavServices = () => {
+  return <div></div>;
+};
+
+export default NavServices;
