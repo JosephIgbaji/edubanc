3f3ae41cfeca226c3c1e80c48395c76ddb3dbb94
JosephIgbaji <igbajijoseph.k@gmail.com>
2024-02-08 22:53:30 +0100

    Loading page corrected

--- src/app/(admin)/admin/page.js
+++ src/app/(admin)/admin/page.js
@@ -8,67 +8,7 @@ import ComponentsHeader from "@/components/admin/ComponentsHeader";
 // import TextEditor from "@/components/admin/TextEditor";
 import TestimonialTable from "@/components/admin/TestimonialTable";
 import TestimonialEditor from "@/components/admin/TestomonialEditor";
-import Loading from "../loading";
-
-// export default function Home() {
-//   const [signIn, setSignIn] = useState(true);
-
-//   const style = "rounded-lg w-full border p-3 block mb-5";
-//   const activeStyle = style + " bg-gray-300 font-bold";
-//   return (
-//     <div>
-//       {!signIn ? (
-//         <Login />
-//       ) : (
-//         <div className="lg:h-screen lg:grid lg:grid-cols-[250px_auto]">
-//           <div className="bg-blue-100 bg-opacity-50 px-4 lg:pt-20">
-//             <button
-//               onClick={() => {
-//                 setCategory("blog");
-//                 setShowBlogEditor(false);
-//                 setShowTestimonialEditor(false);
-//               }}
-//               className={category === "blog" ? activeStyle : style}
-//             >
-//               Blog
-//             </button>
-//             <button
-//               onClick={() => {
-//                 setCategory("testimonials");
-//                 setShowBlogEditor(false);
-//                 setShowTestimonialEditor(false);
-//               }}
-//               className={category !== "blog" ? activeStyle : style}
-//             >
-//               Testimonials
-//             </button>
-//           </div>
-//           <div className="relative px-10 pt-10">
-//             <ComponentsHeader
-//               headers={
-//                 category === "blog" ? componentsObject[0] : componentsObject[1]
-//               }
-//             />
-//             {!showBlogEditor && category === "blog" && <BlogTable />}
-//             {!showTestimonialEditor && category !== "blog" && (
-//               <TestimonialTable />
-//             )}
-//             {showBlogEditor && (
-//               <div className="absolute lg:w-[90%]">
-//                 <TextEditor />
-//               </div>
-//             )}
-//             {showTestimonialEditor && (
-//               <div className="absolute lg:w-[90%]">
-//                 <TestimonialEditor />
-//               </div>
-//             )}
-//           </div>
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
+import Loading from "../Loading";
 
 const Page = () => {
   const [user, setUser] = useState(null);
